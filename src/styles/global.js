a5c410f4d2d8e0d51bae4a41a567aaf4d8500cd6
sabrinaduque <sabrinanduque@hotmail.com>
2023-09-28 15:44:50 -0300

    implements styles global/theme and text fonts

---
+++ src/styles/global.js
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+    color: ${({ theme }) => theme.COLORS.WHITE};
+
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    outline: none;
+  }
+
+  button {
+    font-family: 'Poppins', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 0.2;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.9)
+  }
+`
---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#000A0F", /*fundo da página*/
+    BACKGROUND_800: "##00070A", /*fundo dos cards*/
+    BACKGROUND_700: "##00111A", /*fundo do header/footer */
+    BACKGROUND_600: "#0D1D25", /*fundo dos inputs */
+
+    WHITE: "#F4EDE8",
+    WHITE_100: "#E1E1E6", /*texto dos títulos */
+
+    BLUE: "#0099ff", /*logo e textos azuis*/
+    BLUE_100: "#82F3FF", /*preços dos produtos*/
+
+    GRAY_100: "#C4C4CC", /*texto das descrições*/
+    GRAY_200: "#7C7C8A", /*texto dos Inputs*/
+    GRAY_300: "#4D585E",/*texto do Footer*/
+
+    RED: "#750310",
+  }
+}
