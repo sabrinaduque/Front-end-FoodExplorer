e04eb22bf1b29e3cfca0c193c49a260e5861d002
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-13 17:50:29 -0300

    add responsiveness to button component

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { DEVICE_BREAKPOINTS } from "../../styles/devicesBreakpoints";
 
 export const Container = styled.button`
   background-color: ${({ theme }) => theme.COLORS.RED};
@@ -11,7 +12,12 @@ export const Container = styled.button`
   border: 0;
   gap: 0.8rem;
   border-radius: 0.5rem;
+  padding: 0.7rem 0.7rem;
 
   font-weight: 500;
   font-size: 1.4rem;
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.MD}) {
+    padding: 1.2rem 2.4rem;
+  }
 `
