4a26833f2abcdcdf614c354c1bef2cff43ea4a5c
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-13 21:51:18 -0300

    refactor add React Router DOM and improve indentation

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,11 @@
-import { Container, Content } from "./styles"
+import { Container, Content, NewDish } from "./styles"
+import { Link } from "react-router-dom"
 import { Header } from "../../components/Header"
 import { Footer } from "../../components/Footer"
 import { Button } from "../../components/Button"
-import { ButtonText } from "../../components/ButtonText"
 import { Ingredients } from "../../components/Ingredients"
 import { Counter } from "../../components/Counter"
-import { PiCaretLeftBold, PiReceipt } from 'react-icons/pi'
+import { PiCaretLeftBold, PiReceipt } from "react-icons/pi"
 import saladaRavanello from "../../assets/dishes/mainDishes/SaladaRavanello.png"
 import { useState } from "react"
 
@@ -16,46 +16,46 @@ export function Details() {
     <Container>
       <Header />
       <Content>
-        <ButtonText
-          icon={PiCaretLeftBold}
-          title={"voltar"}
-        />
+        <Link to="/">
+          <PiCaretLeftBold />
+          voltar
+        </Link>
 
-        <div className="infos">
+        <div>
           <img src={saladaRavanello} alt="" />
-          <h2>Salada Ravanello</h2>
-          <span>
-            Rabanetes, folhas verdes e molho agridoce salpicados com gergelim.
-          </span>
 
-          <div className="ingredients">
-            <Ingredients title="alface" />
-            <Ingredients title="cebola" />
-            <Ingredients title="pão naan" />
-            <Ingredients title="pepino" />
-            <Ingredients title="rabanete" />
-            <Ingredients title="tomate" />
+          <div className="infos">
+            <h2>Salada Ravanello</h2>
+            <p>
+              Rabanetes, folhas verdes e molho agridoce salpicados com gergelim.
+            </p>
+
+            <div className="ingredients">
+              <Ingredients title="alface" />
+              <Ingredients title="cebola" />
+              <Ingredients title="pão naan" />
+              <Ingredients title="pepino" />
+              <Ingredients title="rabanete" />
+              <Ingredients title="tomate" />
+            </div>
+
+            {user.isAdmin ? (
+              <div className="confirm">
+                <NewDish to="/new"> Novo prato </NewDish>
+              </div>
+            ) : (
+              <div className="confirm">
+                <Counter />
+                <Button
+                  className="request"
+                  icon={PiReceipt}
+                  title={"pedir ∙ R$ 25,00"}
+                />
+              </div>
+            )}
           </div>
         </div>
-        {
-          user.isAdmin ?
-            <div className="confirm">
-              <Button
-                title={"Editar prato"}
-              />
-            </div>
-            :
-            <div className="confirm">
-              <Counter />
-              <Button
-                icon={PiReceipt}
-                title={"pedir ∙ R$ 25,00"}
-              />
-            </div>
-        }
-
       </Content>
-
       <Footer />
     </Container>
   )
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,22 +1,35 @@
-import styled from "styled-components";
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+import { DEVICE_BREAKPOINTS } from "../../styles/devicesBreakpoints"
 
-export const Container = styled.div `
+export const Container = styled.div`
   width: 100%;
   height: 100vh;
-  font-family: 'Poppins', sans-serif; 
+  font-family: "Poppins", sans-serif;
   display: grid;
   grid-template-rows: 10.5rem auto 7.7rem;
-  grid-template-areas: 
-  "header"
-  "content"
-  "footer";
+  grid-template-areas:
+    "header"
+    "content"
+    "footer";
 `
 
-export const Content = styled.div `
+export const Content = styled.div`
   grid-area: content;
   padding: 2rem 5rem 3.3rem;
 
-  .infos{
+  > a {
+    display: flex;
+    width: fit-content;
+    font-size: 2.4rem;
+    color: ${({ theme }) => theme.COLORS.WHITE_100};
+    background: none;
+    border: none;
+    align-items: center;
+    font-family: "Poppins", sans-serif;
+  }
+
+  > div {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -27,30 +40,131 @@ export const Content = styled.div `
       padding: 1.6rem;
     }
 
-    h2 {
-      color: ${({ theme }) => theme.COLORS.WHITE_100};
-      font-size: 2.5rem;
-      font-weight: 500;
+    .infos {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 2.4rem;
+
+      h2 {
+        color: ${({ theme }) => theme.COLORS.WHITE_100};
+        font-size: 2.5rem;
+        font-weight: 500;
+      }
+
+      > p {
+        display: flex;
+        color: ${({ theme }) => theme.COLORS.WHITE_100};
+        font-size: 1.5rem;
+        justify-content: center;
+        text-align: center;
+      }
+
+      .ingredients {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+      }
     }
 
-    > span {
+    .confirm {
       display: flex;
-      color: ${({ theme }) => theme.COLORS.WHITE_100};
-      font-size: 1.5rem;
-      justify-content: center;
-      text-align: center;
-      padding: 2.3rem 0;
-    }
+      margin: 2rem 0 0.2rem;
 
-    .ingredients {
-      display: flex;
-      justify-content: center;
-      flex-wrap: wrap;
+      span {
+        font-size: 2.2rem;
+      }
     }
   }
 
-  .confirm {
-    display: flex;
-    margin: 4rem 0 0.2rem;
+  @media (min-width: ${DEVICE_BREAKPOINTS.MD}) {
+    margin: 2.4rem 12.1rem;
+
+    > div {
+      height: 100%;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      img {
+        width: 39rem;
+        height: 39rem;
+        margin-right: 4.7rem;
+      }
+
+      .infos {
+        align-items: flex-start;
+        gap: 2.4rem;
+
+        h2 {
+          font-size: 4rem;
+        }
+
+        > p {
+          font-size: 2.4rem;
+          text-align: start;
+        }
+
+        .ingredients {
+          width: 100%;
+          gap: 1.2rem;
+          justify-content: flex-start;
+        }
+
+        .confirm {
+          display: flex;
+          margin: 2.4rem 0 0;
+
+          .request {
+            > svg {
+              display: none;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.LG}) {
+    margin: 1.4rem 2rem;
+    padding: 2rem 5rem 3.3rem;
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      .infos {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 2.4rem;
+
+        > p {
+          display: flex;
+          font-size: 1.5rem;
+          justify-content: center;
+          text-align: center;
+        }
+      }
+    }
+  }
+`
+export const NewDish = styled(Link)`
+  background-color: ${({ theme }) => theme.COLORS.RED};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  align-items: center;
+  justify-content: center;
+  height: 4.8rem;
+  border: 0;
+  border-radius: 0.5rem;
+  display: flex;
+  width: 25.5rem;
+  font-weight: 500;
+  font-size: 1.4rem;
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.LG}) {
+    width: fit-content;
+    padding: 2.4rem;
   }
 `
