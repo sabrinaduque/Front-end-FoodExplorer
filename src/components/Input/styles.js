c2129a1dc0b1343b742f7eb9bfa413d197a51f10
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-13 21:24:19 -0300

    update input component font size and spacing

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,25 +1,27 @@
-import styled from 'styled-components'
+import styled from "styled-components"
 
 export const Container = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
+
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
   color: ${({ theme }) => theme.COLORS.GRAY_200};
 
   border-radius: 0.8rem;
 
   > input {
-    height: 56px;
+    height: 4.8rem;
     width: 100%;
-    padding: 12px;
+    padding: 1.2rem;
+
     color: ${({ theme }) => theme.COLORS.WHITE};
     background: transparent;
     border: 0;
 
     &:placeholder {
-      color: ${({ theme }) => theme.COLORS.GRAY_200};
       font-size: 1.6rem;
+      color: ${({ theme }) => theme.COLORS.GRAY_200};
     }
   }
 
