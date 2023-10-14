c7a44368675ff7ed9f2d33a12b10d48940c31ee9
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-13 21:34:49 -0300

    implements new dish page

---
+++ src/pages/NewDish/index.jsx
@@ -0,0 +1,105 @@
+import { Container, Form, Image, Section } from "./styles"
+import { Header } from "../../components/Header"
+import { Footer } from "../../components/Footer"
+import { IngredientsItens } from "../../components/IngredientsItens"
+import { Link } from "react-router-dom"
+import { Button } from "../../components/Button"
+import { Input } from "../../components/Input"
+import { Textarea } from "../../components/Textarea"
+import { PiUploadSimple, PiCaretLeftBold } from "react-icons/pi"
+
+export function NewDish() {
+  return (
+    <Container>
+      <Header />
+
+      <Form>
+        <header>
+          <Link to="/">
+            <PiCaretLeftBold />
+            voltar
+          </Link>
+
+          <h1>Novo Prato</h1>
+        </header>
+
+        <div className="firstColumn">
+          <Image>
+            <span>Imagem do prato</span>
+            <div>
+              <label htmlFor="dish" className="label">
+                <PiUploadSimple className="react-icon" />
+                Selecione a imagem
+                <Input id="dish" type="file" />
+              </label>
+            </div>
+          </Image>
+
+          <div className="input-wrapper">
+            <label htmlFor="name" className="label">
+              {" "}
+              Nome{" "}
+            </label>
+            <Input id="name" type="text" placeholder="Ex.: Salada Ceasar" />
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="category" className="label">
+              {" "}
+              Categoria{" "}
+            </label>
+            <div className="dishCategory">
+              <select id="category">
+                <option value="default" disabled>
+                  Selecione a categoria
+                </option>
+                <option value="dishes">Pratos</option>
+                <option value="drinks">Bebidas</option>
+                <option value="dessert">Sobremesas</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="secondColumn">
+          <div className="input-wrapper">
+            <label htmlFor="dishIngredients" className="label">
+              {" "}
+              Ingredientes{" "}
+            </label>
+            <Section id="dishIngredients">
+              <div className="ingredients">
+                <IngredientsItens value="React" />
+                <IngredientsItens isNew placeholder="Novo marcador" />
+              </div>
+            </Section>
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="price" className="label">
+              {" "}
+              Preço{" "}
+            </label>
+            <Input id="price" type="text" placeholder="R$ 00,00" />
+          </div>
+        </div>
+
+        <div className="input-wrapper">
+          <label htmlFor="description" className="label">
+            {" "}
+            Descrição{" "}
+          </label>
+          <Textarea
+            id="description"
+            type="text"
+            placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+          />
+        </div>
+        <div className="button">
+          <Button title={"Salvar alterações"} />
+        </div>
+      </Form>
+      <Footer />
+    </Container>
+  )
+}
