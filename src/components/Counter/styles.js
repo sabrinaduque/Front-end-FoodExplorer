32a0c6ae4645d4985ece23b67c9e88221a455bea
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-13 21:47:57 -0300

    improve indentation in the counter component

--- src/components/Counter/styles.js
+++ src/components/Counter/styles.js
@@ -1,16 +1,17 @@
-import styled from 'styled-components'
+import styled from "styled-components"
 
 export const Container = styled.span`
   width: fit-content;
   padding: 0.4rem 0.8rem;
-  font-family: 'Poppins', sans-serif; 
+  font-family: "Poppins", sans-serif;
   color: ${({ theme }) => theme.COLORS.WHITE};
   display: flex;
   align-items: center;
   gap: 1rem;
 
   span {
-    font-size: 2.2rem;
+    font-size: 1.6rem;
+    color: ${({ theme }) => theme.COLORS.WHITE};
   }
 
   svg {
