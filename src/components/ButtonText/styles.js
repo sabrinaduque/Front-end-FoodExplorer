4c85d4532b0516c257418459d1927548c0844a2a
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-19 20:21:05 -0300

    implements responsive button text

--- src/components/ButtonText/styles.js
+++ src/components/ButtonText/styles.js
@@ -1,9 +1,10 @@
 import styled from "styled-components"
+import { DEVICE_BREAKPOINTS } from "../../styles/devicesBreakpoints";
 
 export const Container = styled.button`
   display: flex;
   width: fit-content;
-  font-size: 2.2rem;
+  font-size: 1.6rem;
   color: ${({ theme }) => theme.COLORS.WHITE_100};
   background: none;
   border: none;
@@ -13,5 +14,10 @@ export const Container = styled.button`
   svg {
     width: 2.5rem;
     height: 2.5rem;
+  }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.MD}) {
+    font-size: 2.4rem;
+    font-weight: 700;
   }
 `
