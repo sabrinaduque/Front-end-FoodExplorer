861279678d9d0a7df223d0bbcfaa45b46266a5d9
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-19 20:22:39 -0300

    implements hooks on main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { ThemeProvider } from "styled-components"
-import GlobalStyles from "./styles/global"
-import theme from "./styles/theme"
-import { Details } from './pages/Details'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Routes } from './routes'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+import GlobalStyle from './styles/global'
+import { AuthProvider } from './hooks/auth'
+import { CartProvider } from './hooks/cart'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Details />
+      <GlobalStyle />
+      <AuthProvider>
+        <CartProvider>
+          <Routes />
+        </CartProvider>
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
