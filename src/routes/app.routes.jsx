b476c6e5598e2f898065f69c38b590279b0e440e
sabrinaduque <sabrinanduque@hotmail.com>
2023-10-19 20:18:03 -0300

    refector routes for newDishes and update

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -6,13 +6,14 @@ import { Menu } from "../pages/Menu"
 import { Details } from "../pages/Details"
 import { UpdateDish } from "../pages/UpdateDish"
 
+
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/new" element={<NewDish />} />
+      <Route path="/newDish" element={<NewDish />} />
       <Route path="/details/:id" element={<Details />} />
-      <Route path="/update" element={<UpdateDish />} />
+      <Route path="/update/:id" element={<UpdateDish />} />
       <Route path="/menu" element={<Menu />} />
     </Routes>
   )
